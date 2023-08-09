0cff26a54ca4af632a15bb64904586bb6c5af523
rusbell-24 <rusbell.ruiz.p@gmail.com>
2023-08-09 15:01:06 -0500

    Se agrega la funcionalidad de crear, leer, editar y eliminar

--- app/libreria/views.py
+++ app/libreria/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Libro
+from .forms import LibroForm
 
 # Create your views here.
 
@@ -15,7 +16,22 @@ def libros(request):
     return render(request, 'libros/index.html', {'libros': libros})
 
 def crear(request):
-    return render(request, 'libros/crear.html')
+    formulario = LibroForm(request.POST or None, request.FILES or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros') 
+    return render(request, 'libros/crear.html', {'formulario': formulario})
 
-def editar(request):
-    return render(request, 'libros/editar.html')
+def editar(request, id):
+    libro = Libro.objects.get(id = id)
+    formulario = LibroForm(request.POST or None, request.FILES or None, instance=libro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros') 
+    return render(request, 'libros/editar.html', {'formulario': formulario})
+
+
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
